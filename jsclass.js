3f352054f4359596694a22441048e6c04287beb2
baconstarvation <ant.ucd@gmail.com>
2014-02-25 19:24:37 -0800

    first commit

---
+++ jsclass.js
@@ -0,0 +1,9 @@
+// slides.hackreactor.com
+
+// Objects and Property Access
+// using dot notation add a property name with the value being your actual name
+
+var user = {};
+user.name = "anthony";
+user.age = "26";
+console.log(user);
