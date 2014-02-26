80e5d227c06fe72bd7124bde51fdb67a1fd03a56
baconstarvation <ant.ucd@gmail.com>
2014-02-25 19:47:50 -0800

    now on for-in loop exercises

--- jsclass.js
+++ jsclass.js
@@ -7,3 +7,50 @@ var user = {};
 user.name = "anthony";
 user.age = "26";
 console.log(user);
+
+// add onto the object a property that's your favorite color. set its value to true
+var color = "green";
+user[color] = true;
+console.log(user[color]); // outputs true
+console.log(user["green"]); // outputs true
+
+
+// Iterating and Key Stringification
+
+var obj = {};
+
+obj['x'] = 9;
+obj['0'] = 'cat';
+console.log(obj['x']); // logs 9
+console.log(obj['0']); // logs cat
+
+// The for-in loop
+// you want to use this only for objects
+
+for(var k in obj) {
+	console.log(k); // logs ???. will log either x or 0;
+};
+
+// More for-in loop exercises
+
+/*
+for(var key in obj) {
+	console.log(key); // this will log properties
+	console.log(obj[key]); // this will log the value(s)
+}
+*/
+
+// write a for loop that will print out all the values in the object
+var dog = { bark: "woof", color: "black"};
+
+for(var key in dog) {
+	console.log(dog[key]);
+};
+
+
+
+
+
+
+
+
