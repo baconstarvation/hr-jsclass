60d1d9a86948a3e3c8e30708a4c0543a246c0500
baconstarvation <ant.ucd@gmail.com>
2014-02-25 20:18:48 -0800

    sdjgfkdsjckxvkbvxc

--- jsclass.js
+++ jsclass.js
@@ -3,33 +3,41 @@
 // Objects and Property Access
 // using dot notation add a property name with the value being your actual name
 
+/*
 var user = {};
 user.name = "anthony";
 user.age = "26";
 console.log(user);
+*/
 
 // add onto the object a property that's your favorite color. set its value to true
+/*
 var color = "green";
 user[color] = true;
 console.log(user[color]); // outputs true
 console.log(user["green"]); // outputs true
+*/
 
 
 // Iterating and Key Stringification
 
+/*
 var obj = {};
 
 obj['x'] = 9;
 obj['0'] = 'cat';
 console.log(obj['x']); // logs 9
 console.log(obj['0']); // logs cat
+*/
 
 // The for-in loop
 // you want to use this only for objects
 
+/*
 for(var k in obj) {
 	console.log(k); // logs ???. will log either x or 0;
 };
+*/
 
 // More for-in loop exercises
 
@@ -41,11 +49,47 @@ for(var key in obj) {
 */
 
 // write a for loop that will print out all the values in the object
+
+/*
 var dog = { bark: "woof", color: "black"};
 
 for(var key in dog) {
 	console.log(dog[key]);
 };
+*/
+
+// Arrays
+
+/*
+var arr = [];
+
+arr['x'] = 9;
+arr[ 0 ] = 'cat';
+console.log(arr['x']); // logs 9
+console.log(arr[ 0 ]); // logs 'cat'
+for (var k in arr) {
+	console.log(k); // logs 'x', '0'
+}
+console.log(arr['length']); // logs 1; same as console.log(arr.length)
+*/
+
+var arr = [];
+
+arr['x'] = 9;
+arr[ 0 ] = 'cat', arr[1] = 'dog';
+console.log(arr['x']); // logs 9
+console.log(arr[ 0 ]); // logs 'cat'
+for (var k in arr) {
+	console.log(k); // logs 'x', '0', '1'
+}
+console.log(arr['length']); // logs 2
+
+var x = [1, 2, 3, 4]
+for(var bird = 0; bird < arr.length; bird++) {
+	console.log(x);
+};
+
+
 
 
 
