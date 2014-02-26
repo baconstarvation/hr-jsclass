9637d0dc7df29162052ddb00749efbda0b66a66e
baconstarvation <ant.ucd@gmail.com>
2014-02-25 21:05:00 -0800

    Parameters, Arguments, and the arguments object

--- jsclass.js
+++ jsclass.js
@@ -94,6 +94,7 @@ for(var bird = 0; bird < arr.length; bird++) {
 
 // Functions as Objects
 
+/*
 var fun = function () {
 
 };
@@ -109,10 +110,66 @@ console.log(fun.length);
 for( var i = 0; i < fun.length; i++) {
 	console.log(i);
 }
+*/
 
 
+// Function Invocation
+
+/*
+var fun = function(input) {  // the function definition
+	console.log(input.toUpperCase());
+};
+
+console.log(fun('hi')); // logs 'HI'. invocation is here
+console.log(fun.input); // logs undefined. not a property on the function, it's an argument that's accessible only in those brackets
+*/
 
 
+// Invoking a Nameless Function
+
+/*
+var fun = function() {
+	return 2;
+};
+
+var two = fun();
+console.log(two); // logs 2
+*/
+
+
+// Parameters, Arguments, and the arguments object
+
+/*
+var func = function() { return arguments[0]; };
+console.log(func(1,2,3,4,5));
+*/
+
+/*
+var fun = function(input) {
+	console.log(input);
+}
+fun('hi'); //logs hi
+console.log(fun.arguments); // logs null
+*/
+
+/*
+var fun = function(input) {
+	console.log(arguments);
+	var innerFun = function(input) {
+		console.log(arguments);
+	};	
+	innerFun('yo'); // logs ['yo']
+};
+*/
+
+var x = function(y, z) {
+	var test = "y";
+	console.log(arguments, test);
+	var w = function() {
+		console.log(arguments, test);
+	};
+	w(1);
+};
 
 
 
